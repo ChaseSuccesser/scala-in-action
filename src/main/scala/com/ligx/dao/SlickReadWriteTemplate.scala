767807d85bb15fd8d8ed6cd3e0fd869e3a905ac1
ligx <ligongxing@meituan.com>
2017-03-16 20:41:23 +0800

    add db config

---
+++ src/main/scala/com/ligx/dao/SlickReadWriteTemplate.scala
@@ -0,0 +1,18 @@
+package com.ligx.dao
+
+import com.typesafe.config.ConfigFactory
+import slick.backend.DatabaseConfig
+
+/**
+  * Author: ligongxing.
+  * Date: 2017年03月16日.
+  */
+object SlickReadWriteTemplate {
+  private val config = ConfigFactory.load()
+  val dbConfig = config.getConfig("db")
+
+}
+
+class SlickReadWriteTemplate {
+//  val db = Database.forURI(dbConfig.getString("url"), dbConfig.getString("username"), dbConfig.getString("password"), dbConfig.getString("driver"))
+}
