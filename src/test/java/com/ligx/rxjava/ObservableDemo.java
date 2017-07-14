d380465fabcdea616493eaab030e54591c042878
ligx <ligongxing@meituan.com>
2017-07-14 21:02:30 +0800

    modify observable demo

--- src/test/java/com/ligx/rxjava/ObservableDemo.java
+++ src/test/java/com/ligx/rxjava/ObservableDemo.java
@@ -1,7 +1,9 @@
 package com.ligx.rxjava;
 
 import org.junit.Test;
+import rx.BackpressureOverflow;
 import rx.Observable;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
@@ -14,8 +16,39 @@ import java.util.concurrent.TimeUnit;
 public class ObservableDemo {
 
     @Test
-    public void test(){
+    public void onBackpressureBuffer(){
         Observable.interval(10, TimeUnit.MILLISECONDS)
+                .onBackpressureBuffer(1000, new Action0(){
+                    @Override
+                    public void call() {
+                        System.out.println("overflowed");
+                    }
+                }, BackpressureOverflow.ON_OVERFLOW_ERROR)
+                .observeOn(Schedulers.io())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        System.out.println("received:" + aLong);
+
+                        try {
+                            Thread.sleep(1000L);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void onBackpressureDrop(){
+        Observable.interval(10, TimeUnit.MILLISECONDS)
+                .onBackpressureDrop()
                 .observeOn(Schedulers.io())
                 .subscribe(new Action1<Long>() {
                     @Override
