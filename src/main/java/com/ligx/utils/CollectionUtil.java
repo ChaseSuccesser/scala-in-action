59990d2b266c0fd4dbe9dedb100edd47ff270216
ligx <ligongxing@meituan.com>
2017-11-16 19:00:58 +0800

    修改集合工具类

--- src/main/java/com/ligx/utils/CollectionUtil.java
+++ src/main/java/com/ligx/utils/CollectionUtil.java
@@ -22,22 +22,22 @@ public class CollectionUtil {
      * @return
      */
     public static <T> boolean compareCollection(List<T> list1, List<T> list2) {
-        if (list1 == null && list2 == null) {
+        if (CollectionUtils.isEmpty(list1) && CollectionUtils.isEmpty(list2)) {
             return true;
-        } else if (list1 == null) {
+        } else if (!CollectionUtils.isEmpty(list1) && CollectionUtils.isEmpty(list2)) {
             return false;
-        } else if (list2 == null) {
+        } else if (CollectionUtils.isEmpty(list1) && !CollectionUtils.isEmpty(list2)) {
             return false;
         }
 
         if (list1.size() != list2.size()) {
             return false;
         }
+
         // list1含有只存在list1而不存在list2的元素，删除它们(list1发生了改变)，返回true
         // list1没有只存在list1而不存在list2中元素，list1不改变，返回false
         return !list1.retainAll(list2);
     }
-
 
     /**
      * 比较两个List的集合元素是否完全相同
@@ -50,27 +50,29 @@ public class CollectionUtil {
      * @return
      */
     public static <T, U> boolean compareCollection(List<T> list1, List<U> list2, BiFunction<T, U, Boolean> compareFunc) {
-        if (list1 != null) {
-            if (list2 == null) {
-                return false;
-            } else if (list1.size() != list2.size()) {
-                return false;
-            } else {
-                int size = list1.size();
-                int equalCount = 0;
-                for (T t : list1) {
-                    for (U u : list2) {
-                        if (compareFunc.apply(t, u)) {
-                            equalCount += 1;
-                            break;
-                        }
-                    }
-                }
-                return equalCount == size;
-            }
-        } else {
-            return list2 == null;
+        if (CollectionUtils.isEmpty(list1) && CollectionUtils.isEmpty(list2)) {
+            return true;
+        } else if (!CollectionUtils.isEmpty(list1) && CollectionUtils.isEmpty(list2)) {
+            return false;
+        } else if (CollectionUtils.isEmpty(list1) && !CollectionUtils.isEmpty(list2)) {
+            return false;
         }
+
+        if (list1.size() != list2.size()) {
+            return false;
+        }
+
+        int size = list1.size();
+        int equalCount = 0;
+        for (T t : list1) {
+            for (U u : list2) {
+                if (compareFunc.apply(t, u)) {
+                    equalCount += 1;
+                    break;
+                }
+            }
+        }
+        return equalCount == size;
     }
 
 
