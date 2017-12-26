73282810976c131b22a4b142c428f058cec900e0
ligongxing <ligongxing@meituan.com>
2017-12-26 22:36:42 +0800

    unit test Hello macro

--- core/src/test/scala/com/ligx/macros/MacrosSpec.scala
+++ core/src/test/scala/com/ligx/macros/MacrosSpec.scala
@@ -8,6 +8,11 @@ import org.scalatest.{FlatSpec, Matchers}
   * Date: 2017年12月26日.
   */
 class MacrosSpec extends FlatSpec with Matchers {
+  import com.ligx.hello.HelloMacro._
+
+  "Hello macro" should "" in {
+    println(greeting)
+  }
 
   "Benchmark macro annotation" should "" in {
     println(testMethodWithArgs(1L, 2L))
